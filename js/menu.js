c615dcab3434db6691640931812afbbf825c9b48
amine <saissihassaniamine@gmail.com>
2021-02-23 10:10:35 +0100

    update

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,7 @@
-var checkbox = document.querySelector("input[name=checkbox]");
-
-checkbox.addEventListener('change', function() {
-    if (this.checked) {
+function menu(me) {
+    if (me.checked) {
         document.getElementById("row").style.display = "block";
     } else {
         document.getElementById("row").style.display = "none";
     }
-});
+}
